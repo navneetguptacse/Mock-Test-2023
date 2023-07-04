dd440982ddc11c81c0c01fbadf0c9ad5b36a1c59
Navneet Gupta <88278303+navneetguptacse@users.noreply.github.com>
2023-07-04 12:07:29 +0530

    Add files via upload

---
+++ Mock Test 1 - Data Science - PPT - PW Skills/19.py
@@ -0,0 +1,19 @@
+# 19. Write a function called ‘calculate_mean’ that takes a list of numbers as input and returns the mean (average) of the numbers. The function should calculate the mean using the sum of the numbers divided by the total count.
+
+# Example:
+
+# Code: Python function
+
+def calculate_mean(numbers):
+    total = sum(numbers)
+    count = len(numbers)
+    mean = total / count
+    return mean
+
+
+data = [10, 15, 20, 25, 30]
+mean_value = calculate_mean(data)
+print("Mean:", mean_value)
+
+# Output:
+# Mean: 20.0
